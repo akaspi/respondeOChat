b2fa9f42bcbea71596bc3aab09fdab67e7c3c17b
Amit_Kaspi <akaspi@wix.com>
2016-04-06 17:36:51 +0300

    App.jsx cleanup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+import Chat from './Chat.jsx';
+
+export default React.createClass({
+    displayName: 'App',
+    render() {
+        return (
+            <Chat/>
+        );
+    }
+});
